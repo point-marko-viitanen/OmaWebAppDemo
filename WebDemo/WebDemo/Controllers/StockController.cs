10df07fd4e8ca616f6a047b11100b3107e163644
Marko Viitanen <marko.viitanen@stud.pointcollege.fi>
2016-11-07 10:39:51 +0200

    Uusin versio

---
+++ WebDemo/WebDemo/Controllers/StockController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDemo.Database;
+using WebDemo.Models;
+
+namespace WebDemo.Controllers
+{
+    public class StockController : Controller
+    {
+        // GET: Stock
+        public ActionResult Index()
+        {
+            List<InventoryViewModel> model = new List<InventoryViewModel>();
+            JalkahoitolaDemoEntities entities = new JalkahoitolaDemoEntities();
+            try
+            {
+                List<ItemGroupName> stocks = entities.ItemGroupNames.ToList();
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+            return View(model);
+        }
+
+      
+        public ActionResult List()
+        {
+           
+            return View();
+        }
+
+        // GET: Stock/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stock/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Stock/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Stock/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Stock/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Stock/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebDemo/WebDemo/Models/InventoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDemo.Models
+{
+    class InventoryViewModel
+    {
+        public string ProductName { get; set; }
+        public int Stock { get; set; }
+        public int Prices { get; set; }
+        public DateTime ExpDate { get; set; }
+
+    }
+}
